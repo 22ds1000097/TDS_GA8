322dd2d8f3d4df905034d204bfbce7cdac83b38e
22ds1000097 <109918695+22ds1000097@users.noreply.github.com>
2022-07-25 00:07:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
+
+echo "\
+[genera]\n\
+email = \"22ds1000097@student.onlinedegree.iitm.ac.in\"\n\
+"> ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-port = $PORT\n\
+enableXsrfProtection = false\n\
 enableCORS = false\n\
-\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
